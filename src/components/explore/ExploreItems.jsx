3db7c8e9f43ade1ec46a331edfd15db906605c58
samtimeinc <samtime.worldwide@gmail.com>
2026-01-22 20:18:18 -0800

    filter is done via baackend

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 import Skeleton from "../UI/Skeleton";
 import UserImagePlaceholder from "../UI/UserImagePlaceholder";
@@ -12,8 +10,12 @@ const ExploreItems = () => {
   const [visibleItems, setVisibleItems] = useState(8);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  async function fetchItems() {
-    const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore");
+  async function fetchItems(filter = "") {
+    let api = "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+    if (filter !== "") {
+      api += `?filter=${filter}`
+    }
+    const { data } = await axios.get(api);
     setItems(data || [])
   }
 
@@ -85,8 +87,7 @@ const ExploreItems = () => {
     } else {
       skeletonHeight = "440px"
     }
-
-    return new Array({visibleItems}).fill(0).map((_, index) => (
+    return new Array(8).fill(0).map((_, index) => (
       <div
         key={index}
         className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -96,13 +97,20 @@ const ExploreItems = () => {
       </div>
   ))}
 
+  function handleFilter(event) {
+    let filter = event.target.value;
+    setVisibleItems(8)
+    setItems([])
+    fetchItems(filter);
+  }
+
   function handleLoadMore() {
     setVisibleItems(prevVisible => prevVisible + 4);
   }
 
   useEffect(() => {
     fetchItems();
-  }, [visibleItems])
+  }, [])
 
   useEffect(() => {
     setWindowWidth(window.innerWidth);
@@ -111,7 +119,7 @@ const ExploreItems = () => {
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select id="filter-items" defaultValue="" onChange={handleFilter} >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
@@ -123,60 +131,6 @@ const ExploreItems = () => {
       ) : (
         renderSkeletonItems()
       )}
-      {/* {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))} */}
       <div className="col-md-12 text-center">
         {visibleItems < items.length && (
           <button 
